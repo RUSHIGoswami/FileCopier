0e97d6f9e0561e1766443d4e51fc222a13f7232d
Rushi M Goswami <64486280+RUSHIGoswami@users.noreply.github.com>
2023-01-24 12:52:17 +0530

    Initial Commit
    As my first commit, I have uploaded a folder containing my java code file and text files on which I performed copy operation.

---
+++ Bonus_Assignment_1/fileCopier.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class fileCopier{
+    public static void copyFile(String src, String dest, String fileName) throws FileNotFoundException, IOException {
+        File input = new File(src+"\\"+fileName);
+        File output = new File(dest+"\\"+fileName);
+        File temp=null;
+        FileReader reading = new FileReader(input);
+        FileWriter writing = null;
+        if(input.exists()){
+           int a;
+           if(output.exists()){
+               temp = new File(dest+"\\"+fileName.substring(0,fileName.indexOf("."))+"-copy"+fileName.substring(fileName.indexOf("."), fileName.length()));
+               if(temp.exists()){
+                   int i = 2;
+                   while(true){
+                       temp = new File(dest+"\\"+fileName.substring(0,fileName.indexOf("."))+"-copy("+i+")"+fileName.substring(fileName.indexOf("."), fileName.length()));
+                       if(!temp.exists())
+                           break;
+                       i++;
+                   }
+                   writing = new FileWriter(dest+"\\"+fileName.substring(0, fileName.indexOf("."))+"-copy("+i+")"+fileName.substring(fileName.indexOf("."), fileName.length()));    
+                   while((a=reading.read()) != -1){
+                       writing.write(a);
+                   }
+                   System.out.println("Successfully copied");
+               }else{
+                   writing = new FileWriter(dest+"\\"+fileName.substring(0,fileName.indexOf("."))+"-copy"+fileName.substring(fileName.indexOf("."), fileName.length()));
+                   while((a=reading.read()) != -1){
+                       writing.write(a);
+                   }
+                   System.out.println("Successfully copied");
+               }
+           }else{
+               writing = new FileWriter(dest+"\\"+fileName);
+               while((a=reading.read()) != -1){
+                   writing.write(a);
+               }
+               System.out.println("Successfully copied");
+           }
+       }else{
+           System.out.println("File does not exist");
+       }
+       reading.close();
+       writing.close();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter path of source file :- ");
+        String src = sc.nextLine();
+        System.out.print("Enter path of destination :- ");
+        String dest = sc.nextLine();
+        System.out.print("Enter file name with extension :- ");
+        String fileName = sc.nextLine();
+        copyFile(src, dest, fileName);
+        sc.close();
+    }
+}
